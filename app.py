2a3f3f193cedf55001da62203474729cf7bf012b
student <student@ACCESS-F6NCX33.ad.dkit.ie>
2025-09-05 21:32:12 +0100

    Descrive your change

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+# Home page
 @app.route('/')
 def home():
-    return "Hello"
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return "This is the About page"
+# Hello route
+@app.route('/hello/<name>')
+def hello(name):
+    return f"Hello, {name}!"
 
+# Simple API POST route
+@app.route('/api/data', methods=['POST'])
+def api_data():
+    data = request.json
+    return jsonify({"received": data})
 
 if __name__ == "__main__":
     app.run(debug=True)
